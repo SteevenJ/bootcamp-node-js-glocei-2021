79d9ebe17be3bb96b48c682af06687c7f3a99740
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-09-29 23:16:28 -0500

    S3V12 Add chalk module

--- section_3/notes-app/app.js
+++ section_3/notes-app/app.js
@@ -1,8 +1,11 @@
 //require('./utils.js')
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes')
 
 console.log(getNotes())
 
 console.log(validator.isEmail('carlos@gmail.com'))
 console.log(validator.isURL('https://www.npmjs.com/package/validator'))
+
+console.log(chalk.green('Hello world!'));
