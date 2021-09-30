6306c57d3ad175244944115b80b6b382f89a1852
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-09-29 23:47:03 -0500

    S4V16 Yargs part I

--- section_4/notes-app/app.js
+++ section_4/notes-app/app.js
@@ -1,10 +1,46 @@
+const yargs = require('yargs')
+
+// Customize yargs version
+yargs.version('1.1.0')
 
 
-const command = process.argv[2]
-if(command === 'add'){
-    console.log('Adding note!')
-} else if(command === 'remove'){
-    console.log('Removing note!')
-} else{
-    console.log("NN: " + process.argv[2])
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note')
+    }
+})
+
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function(){
+        console.log('Listing all notes')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
