77e5e9a6d2e5b6cf2500b7e02ceb51723969127f
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-10-01 17:19:57 -0500

    S6V34 Errors

--- section_6/weather-app/app.js
+++ section_6/weather-app/app.js
@@ -4,18 +4,34 @@ const url = "http://api.weatherstack.com/current?access_key=45eccb00258088305248
 
 request({url: url, json: true}, (error, response) => {
 
-    const current = response.body.current
-    console.log(current.weather_descriptions[0])
-    console.log("It is currently " + current.temperature + 'º out. There is a ' + current.precip + '% chance of rain')
+    if(error){
+        console.log(error)
+    }else if(response.body.error){
+        console.log("Error on body"+ response.body.error)
+    }else{
+        const current = response.body.current
+        console.log(current.weather_descriptions[0])
+        console.log("It is currently " + current.temperature + 'º out. There is a ' + current.precip + '% chance of rain')
+    }
+    
 })
 
 //Geocoding
 // Address ->Lat/Ling -> Weather
 
-const urlGC = "https://api.mapbox.com/geocoding/v5/mapbox.places/Medellin.json?access_token=pk.eyJ1Ijoic3RlZXZlbmphcmFtaWxsbyIsImEiOiJja3U4dzd2Z2swMTBuMnJwMmhlcHR0a3lzIn0.3XKi23wPRz_OwXCwE3enJw&languaje=es&limit=1"
+const urlGC = "https://api.mapbox.com/geocoding/v5/mapbox.places/1213yuer.json?access_token=pk.eyJ1Ijoic3RlZXZlbmphcmFtaWxsbyIsImEiOiJja3U4dzd2Z2swMTBuMnJwMmhlcHR0a3lzIn0.3XKi23wPRz_OwXCwE3enJw&languaje=es&limit=1"
 
 request({url: urlGC, json: true}, (error, response) => {
-
-    const features = response.body.features
-    console.log("Lat: " + features[0].center[1] + ", Long: " + features[0].center[0])
+    if(error){
+        console.log(error)
+    }else if(response.body.mensaje){
+        console.log("Error on body"+ response.body.error)
+    }else{
+        const features = response.body.features
+        if(features.length === 0){
+            console.log("Place not found")
+        }else{
+            console.log("Lat: " + features[0].center[1] + ", Long: " + features[0].center[0])
+        }
+    }
 })
