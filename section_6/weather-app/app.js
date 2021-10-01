537124d98b2ee44147f5465fb40ff0f2ea641989
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-10-01 17:49:56 -0500

    S5V36 Callbaks functions

--- section_6/weather-app/app.js
+++ section_6/weather-app/app.js
@@ -1,6 +1,7 @@
 const request = require('postman-request')
+const geocode = require('./utils/geocode')
 
-const url = "http://api.weatherstack.com/current?access_key=45eccb002580883052488f7a4a063ed3&query=Bogota&units=m"
+/* const url = "http://api.weatherstack.com/current?access_key=45eccb002580883052488f7a4a063ed3&query=Bogota&units=m"
 
 request({url: url, json: true}, (error, response) => {
 
@@ -15,23 +16,9 @@ request({url: url, json: true}, (error, response) => {
     }
     
 })
+*/
 
-//Geocoding
-// Address ->Lat/Ling -> Weather
-
-const urlGC = "https://api.mapbox.com/geocoding/v5/mapbox.places/1213yuer.json?access_token=pk.eyJ1Ijoic3RlZXZlbmphcmFtaWxsbyIsImEiOiJja3U4dzd2Z2swMTBuMnJwMmhlcHR0a3lzIn0.3XKi23wPRz_OwXCwE3enJw&languaje=es&limit=1"
-
-request({url: urlGC, json: true}, (error, response) => {
-    if(error){
-        console.log(error)
-    }else if(response.body.mensaje){
-        console.log("Error on body"+ response.body.error)
-    }else{
-        const features = response.body.features
-        if(features.length === 0){
-            console.log("Place not found")
-        }else{
-            console.log("Lat: " + features[0].center[1] + ", Long: " + features[0].center[0])
-        }
-    }
+geocode('Medellin', (error, data) => {
+    console.log(error)
+    console.log(data)
 })
