9ad79476262560e974b96e821445f74039711654
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-10-01 16:40:57 -0500

    S6V32 Custom http requests

--- section_6/weather-app/app.js
+++ section_6/weather-app/app.js
@@ -1,8 +1,11 @@
 const request = require('postman-request')
 
-const url = "http://api.weatherstack.com/current?access_key=45eccb002580883052488f7a4a063ed3&query=New%20York"
+const url = "http://api.weatherstack.com/current?access_key=45eccb002580883052488f7a4a063ed3&query=Bogota&units=m"
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    //console.log()
+
+    const current = response.body.current
+    console.log(current.weather_descriptions[0])
+    console.log("It is currently " + current.temperature + 'º out. There is a ' + current.precip + '% chance of rain')
 })
