fc8e21925460ccdd9c1674da8412874f09e89287
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-10-03 12:43:41 -0500

    S8V6 Form, submit and fetch

--- section_8/web-server/public/js/main.js
+++ section_8/web-server/public/js/main.js
@@ -1,33 +1,23 @@
 console.log("Hello from JS static file");
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    })
-})
+const weatherForm = document.querySelector('form')
+const weatherInput = document.querySelector('input')
 
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-fetch('http://api.weatherstack.com/current?access_key=45eccb002580883052488f7a4a063ed3&query=Boston').then((response) => {
-    response.json().then(({success = true, error = undefined, request = {}, location= {}, current = {} } = {}) => {
-        if(!success && error){
-            console.log(error.info)
-        }else{
-            if(current.length == 0){
-                console.log("Coordinates not found")
-            }else{
-                console.log(current.weather_descriptions[0] + " .It is currently " + current.temperature + 'º out. There is a ' + current.precip + '% chance of rain')
-            }
+    if(weatherInput){
+        if(weatherInput != ''){
+            fetch('http://localhost:3000/weather?address=' + weatherInput.value).then((response) => {
+            response.json().then(({error = undefined, location = undefined, forecast = undefined } = {}) => {
+                if(error){
+                    console.log(error)
+                }else{
+                    console.log(location)
+                    console.log(forecast)
+                }
+            })
+        })
         }
-    })
-})
-
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then(({error = undefined, location = undefined, forecast = undefined } = {}) => {
-        if(error){
-            console.log(error)
-        }else{
-            console.log(location)
-            console.log(forecast)
-        }
-    })
+    }
 })
