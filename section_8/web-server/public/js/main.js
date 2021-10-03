4361d1c93d252f9a05fddec35f945e81254758c5
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-10-03 12:58:01 -0500

    S8V7 Render info on the screen

--- section_8/web-server/public/js/main.js
+++ section_8/web-server/public/js/main.js
@@ -2,19 +2,24 @@ console.log("Hello from JS static file");
 
 const weatherForm = document.querySelector('form')
 const weatherInput = document.querySelector('input')
+const p1 = document.querySelector('#p1')
+const p2 = document.querySelector('#p2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    p1.textContent = "Loading..."
+    p2.textContent = ""
 
     if(weatherInput){
         if(weatherInput != ''){
             fetch('http://localhost:3000/weather?address=' + weatherInput.value).then((response) => {
             response.json().then(({error = undefined, location = undefined, forecast = undefined } = {}) => {
                 if(error){
-                    console.log(error)
+                    p1.textContent = error
                 }else{
-                    console.log(location)
-                    console.log(forecast)
+                    p1.textContent = location
+                    p2.textContent = forecast
                 }
             })
         })
