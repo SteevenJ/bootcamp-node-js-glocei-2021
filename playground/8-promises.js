8a43100d5fec5f6808536e3f161f12ca95b3f7f6
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-10-12 17:21:25 -0500

    Promise on promise

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,4 +1,4 @@
-const doWorkPromise = new Promise((resolve, reject) => {
+/* const doWorkPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         reject('This is my error!')
         //resolve([1, 4, 7])
@@ -9,7 +9,7 @@ doWorkPromise.then((result) => {
     console.log('Success', result)
 }).catch((error) => {
     console.log('Error', error);
-})
+}) */
 
 //
 //                                  fulfilled
@@ -18,3 +18,23 @@ doWorkPromise.then((result) => {
 //                              \
 //                                  rejected
 //
+
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+add(1,2).then((sum) => {
+    console.log(sum);
+    add(sum, 5).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    })
+}).catch((error) => {
+    console.log(error);
+})
