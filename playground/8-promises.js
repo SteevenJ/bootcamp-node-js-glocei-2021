202e6f8e617420cdd79ad860615e18aee2c6151e
Carlos Steeven Jaramillo Github <c.steeven@hotmail.com>
2021-10-12 17:24:58 -0500

    Promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -28,13 +28,29 @@ const add = (a, b) => {
     })
 }
 
-add(1,2).then((sum) => {
+/* add(1,2).then((sum) => {
     console.log(sum);
     add(sum, 5).then((data) => {
         console.log(data);
     }).catch((error) => {
         console.log(error);
     })
+}).catch((error) => {
+    console.log(error);
+}) */
+
+
+add(1, 1).then((sum) => {
+    console.log(sum)
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
+    return add(sum2, 7)
+}).then((sum3) => {
+    console.log(sum3)
+    return add(sum3, 710)
+}).then((sum4) => {
+    console.log(sum4)
 }).catch((error) => {
     console.log(error);
 })
